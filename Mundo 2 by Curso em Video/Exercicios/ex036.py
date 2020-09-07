e365161ad500ba1ad8737afc4f8a558d62a27ba2
kilerhg <64300982+kilerhg@users.noreply.github.com>
2020-09-06 21:34:26 -0300

    Upload Aulas

---
+++ Mundo 2 by Curso em Video/Exercicios/ex036.py
@@ -0,0 +1,12 @@
+# Receba o valor da casa, salario e em quantos anos e verifique se a prestação mensal excede 30%, caso sim o emprestimo sera negado
+
+vcasa = float(input('Digite o valor da casa:'))
+salario = float(input('Digite o seu salario:'))
+anos = int(input('Digite em quantos anos deseja pagar'))
+
+prestacao = vcasa / anos
+
+if ((salario * 0.7)*12) > prestacao:
+    print('Emprestimo Aceito')
+else:
+    print('Emprestimo Negado')
