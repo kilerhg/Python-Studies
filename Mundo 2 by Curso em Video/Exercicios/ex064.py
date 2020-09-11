723049d337a249ccff48d5a6054db09676c305ac
kilerhg <64300982+kilerhg@users.noreply.github.com>
2020-09-11 15:29:00 -0300

    Upload Exercicios

---
+++ Mundo 2 by Curso em Video/Exercicios/ex064.py
@@ -0,0 +1,8 @@
+# Leia varios numeros inteiros so parar quando digitar 999, no final mostre quantos numeros foram digitados e qual foi a soma entre eles desconsiderando o flag
+cn = 0
+n = 0
+while cn != 999:
+    cn = int(input('Digite um numero Inteiro:'))
+    if cn != 999:
+        n += cn
+print(n)
---
+++ Mundo 2 by Curso em Video/Exercicios/ex065.py
@@ -0,0 +1,13 @@
+# Leia varios numeros inteiros pelo teclado, no final da execução, mostre a media e qual foi o maior e o menor. o programa deve perguntar ao usuario se ele quer continuar ou não a digitar valores
+flag = 'a'
+sm = 0
+ct = 0
+ls = []
+while flag not in 'Ss':
+    print('= ='*15)
+    n = int(input('Digite um numero:'))
+    flag = str(input('Deseja parar? [S/N]:'))
+    sm += n
+    ct += 1
+    ls.append(n)
+print(f'A media dos Numeros Digitados é: {sm/ct}\nO maior numero digitado é:{max(ls)}\nO menor Numero Digitado: {min(ls)}')
