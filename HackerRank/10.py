48c382687e0604dede9b124d2cef15a9bdbbcb4a
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2020-10-14 23:57:08 -0300

    Upload Exercicos feitos

--- HackerRank/10.py
+++ HackerRank/10.py
@@ -1,1 +1,8 @@
 # 
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    for x in range(0,arr.count(max(arr))):
+        arr.remove(max(arr))
+    print(max(arr))
--- HackerRank/11.py
+++ HackerRank/11.py
@@ -1,1 +1,24 @@
 # 
+if __name__ == '__main__':
+    pessoas = []
+    pessoa = []
+    scores = []
+    penultimo = []
+    for x in range(int(input().strip())):
+        name = input().strip()
+        score = float(input().strip())
+        pessoa.append(name)
+        pessoa.append(score)
+        scores.append(score)
+        pessoas.append(pessoa[:])
+        pessoa.clear()
+    for x in range(scores.count(min(scores))):
+        scores.remove(min(scores))
+    scores.sort()
+    for x in range(scores.count(min(scores))):
+        penultimo.append(scores[x])
+    if pessoas[0][0] != 'Test1':
+        pessoas.reverse()
+    for x in pessoas:
+        if x[1] == penultimo[0]:
+            print(x[0])
