1cf106c09558b67996819e7bdfac39d108674542
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2020-11-24 12:45:03 -0300

    Update Tela de Login
    Adicionado as Funcionalidade de
    
    * Login
    * Deletar Registro
    * Redefinir Senha
    
    Sendo agora um CRUD

--- SQLite/Projetos/Tela_Login/janela.py
+++ SQLite/Projetos/Tela_Login/janela.py
@@ -2,10 +2,11 @@ from interface import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QClipboard
 from PyQt5.QtWidgets import QApplication, QMessageBox
+import sys
 import sqlite3
 
-def main(ui):
 
+def main(ui):
     try:
         banco = sqlite3.connect('banco_dados.db')
         cursor = banco.cursor()
@@ -18,6 +19,15 @@ def main(ui):
     except:
         pass
 
+    def nivel_acesso(usuario):
+        usuario = str(usuario)
+        if usuario == 'adm':
+            nivel = 2
+        elif usuario == 'visitante':
+            nivel = 0
+        else:
+            nivel = 1
+        return nivel
     def limpar_campos_login():
         ui.txt_usuario.setText('')
         ui.txt_senha.setText('')
@@ -25,6 +35,13 @@ def main(ui):
     def limpar_campos_cadastrar():
         ui.txt_cadastrar_usuario.setText('')
         ui.txt_cadastrar_senha.setText('')
+
+    def limpar_campos_novasenha():
+        ui.txt_novasenha_usuario.setText('')
+        ui.txt_novasenha_senha.setText('')
+
+    def limpar_campos_removerconta():
+        ui.txt_removerconta_usuario.setText('')
 
     def mostrar_msg(titulo,texto):
         titulo = str(titulo)
@@ -51,7 +68,31 @@ def main(ui):
         limpar_campos_login()
         if len(resultado) != 0:
            mostrar_msg('Login Empresa XYZ',f'Olá {usuario}, Bem Vindo')
-           #  print(f'Olá {usuario}, Bem Vindo')
+           ui.frame.lower()
+           ui.frame_3.lower()
+           ui.frame_4.lower()
+           ui.lb_nomeusuario.setText(f'{usuario}')
+           acesso = nivel_acesso(usuario)
+           ui.lb_nivel_acesso.setText(f'{acesso}')
+           if acesso == 0:
+               ui.formFrame_4.enabled = False
+               ui.formFrame_4.hide()
+               ui.lb_nomeusuario_2.hide()
+               ui.btn_enviar_removerconta.enabled = False
+               ui.btn_enviar_removerconta.hide()
+           elif acesso == 1:
+               ui.formFrame_4.enabled = False
+               ui.formFrame_4.hide()
+               ui.lb_nomeusuario_2.hide()
+               ui.btn_enviar_removerconta.enabled = False
+               ui.btn_enviar_removerconta.hide()
+           else:
+               ui.formFrame_4.enabled = True
+               ui.formFrame_4.show()
+               ui.lb_nomeusuario_2.show()
+               ui.btn_enviar_removerconta.enabled = True
+               ui.btn_enviar_removerconta.show()
+
         else:
             mostrar_msg('Login Empresa XYZ','Algo deu errado!! Digite a senha ou usuario Corretamente')
             # print('Algo deu errado!! Digite a senha ou usuario Corretamente')
@@ -80,14 +121,62 @@ def main(ui):
         banco.close()
         limpar_campos_cadastrar()
 
+    def func_novasenha():
+
+        usuario_novasenha = str(ui.txt_novasenha_usuario.text()).strip()
+        senha_novasenha = str(ui.txt_novasenha_senha.text()).strip()
+
+        banco = sqlite3.connect('banco_dados.db')
+        cursor = banco.cursor()
+
+        try:
+            cursor.execute(f"SELECT * FROM conta WHERE login = '{usuario_novasenha}'")
+            resultado = cursor.fetchall()
+            banco.commit()
+            if len(resultado) != 0:
+                cursor.execute(f"UPDATE conta SET senha = '{senha_novasenha}' WHERE login = '{usuario_novasenha}'")
+                banco.commit()
+                mostrar_msg('Redefinição de Senha Empresa XYZ','Senha redefinida com sucesso')
+            else:
+                mostrar_msg('Redefinição de Senha Empresa XYZ','Usuario Não Encontrado no banco de dados')
+        except sqlite3.Error as erro:
+            print(f'Algo deu errado!! Erro de nome : {erro}')
+        banco.close()
+        limpar_campos_novasenha()
+
+    def func_remover_conta():
+        #     cursor.execute("DELETE from pessoas WHERE idade = as")
+        usuario_remover = str(ui.txt_removerconta_usuario.text()).strip()
+
+        banco = sqlite3.connect('banco_dados.db')
+        cursor = banco.cursor()
+        try:
+            cursor.execute(f"SELECT * from conta WHERE login = '{usuario_remover}'")
+            resultado = cursor.fetchall()
+            banco.commit()
+            if len(resultado) != 0:
+                if usuario_remover != 'adm':
+                    cursor.execute(f"DELETE from conta WHERE login = '{usuario_remover}'")
+                    banco.commit()
+                    mostrar_msg('Removendo Contas Empresa XYZ',f'Conta {usuario_remover} Removida Com Sucesso!!!')
+                else:
+                    mostrar_msg('Removendo Contas Empresa XYZ','Você Não Pode Remover a Conta de Administrador')
+            else:
+                mostrar_msg('Removendo Contas Empresa XYZ','Usuario Não Encontrado em Nossa base de dados')
+        except sqlite3.Error as erro:
+            mostrar_msg('Removendo Contas Empresa XYZ',f'Algo deu errado!! Erro de nome : {erro}')
+        banco.close()
+        limpar_campos_removerconta()
     ui.btn_enviar.clicked.connect(func_login)
     ui.btn_enviar_4.clicked.connect(func_cadastrar)
+    ui.btn_enviar_novasenha.clicked.connect(func_novasenha)
+    ui.btn_enviar_removerconta.clicked.connect(func_remover_conta)
     ui.txt_senha.setEchoMode(QtWidgets.QLineEdit.Password)
     ui.txt_cadastrar_senha.setEchoMode(QtWidgets.QLineEdit.Password)
+    ui.txt_novasenha_senha.setEchoMode(QtWidgets.QLineEdit.Password)
 
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
