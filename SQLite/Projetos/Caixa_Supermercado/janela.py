bd0c92d6acb16bfa263c3ffb473fd5fdbb9c23df
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2020-12-13 16:05:03 -0300

    Atualização

--- SQLite/Projetos/Caixa_Supermercado/janela.py
+++ SQLite/Projetos/Caixa_Supermercado/janela.py
@@ -11,11 +11,55 @@ from datetime import datetime
 def main(ui):
     produtos = ['000001', 'PIPOCA', '2.59', '000002', 'MANTEIGA', '4.20']
 
+    try:
+        # Criando Banco de dados e Tabelas
+        banco = sqlite3.connect('banco_dados.db')
+        cursor = banco.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS produtos (
+        id integer PRIMARY KEY AUTOINCREMENT,
+        codigo integer,
+        produto text,
+        preco REAL)""")
+        banco.commit()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS notas (
+        id integer PRIMARY KEY AUTOINCREMENT,
+        caixa integer,
+        nota text,
+        valor real,
+        data text)""")
+        banco.commit()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS gestao (
+        id integer PRIMARY KEY AUTOINCREMENT,
+        caixa integer,
+        codigo integer,
+        quantidade integer,
+        preco real,
+        data text)""")
+        banco.commit()
+        banco.close()
+    except sqlite3.Error as erro:
+        print(f'Erro de nome: {erro}')
+
+    def data_agora():
+        data = str(datetime.today())
+        data = data[:-7]
+        return data
+
+    def mostrar_msg(titulo, texto):
+        titulo = str(titulo)
+        texto = str(texto)
+        win = MainWindow
+        msg = QMessageBox(win)
+        msg.setWindowTitle(f"{titulo}")
+        msg.setText(f"{texto}")
+        x = msg.exec_()
+
     def definir_troco():
         total_compra = float(ui.txt_preco.text())
         dinheiro_recebido = float(ui.txt_dinheiro_recebido.text())
         troco = dinheiro_recebido - total_compra
-        ui.txt_troco.setText(f'{troco}')
+        ui.txt_troco.setText(f'{troco:.2f}')
+
     def definir_preco_total(preco,quantidade,opcao=0):
         if opcao == 1:
             pass
@@ -30,6 +74,7 @@ def main(ui):
                 valor_atual = float(valor_atual)
             calculo = valor_atual+valor_adicionar
             ui.txt_preco.setText(f'{calculo:.2f}')
+
     def procurar():
         global codigo,produto,preco
         try:
@@ -37,20 +82,22 @@ def main(ui):
             if len(pesquisar) != 0:
                 if len(str(pesquisar)) < 6:
                     pesquisar = ('0' * (6 - len(str(pesquisar)))) + str(pesquisar)
+
                 codigo_indice = produtos.index(f'{pesquisar}')
                 codigo = produtos[codigo_indice]
                 produto = produtos[codigo_indice + 1]
-                # quantidade = produtos[codigo_indice + 2]
                 preco = produtos[codigo_indice + 2]
+
                 ui.txt_codigo_produto.setText(f'{codigo}')
                 ui.txt_produto.setText(f'{produto}')
                 ui.txt_preco_unitario.setText(f'{preco}')
             else:
-                print('teste')
-        except:
-            print('erro')
+                mostrar_msg('Caixa EMPRESA XYZ','Digite o codigo do produto')
+        except ValueError as erro:
+            mostrar_msg('Caixa EMPRESA XYZ',f'Erro nome: {erro}')
+
     def adicionar_linha_nota(codigo_produto,produto,quantidade,preco):
-        ui.txt_nota.append(f'{codigo_produto:0<6} {"":-<4} {produto: <10} {"":-<4} {quantidade:0>3} {"-" * 3} {preco: >7}')
+        ui.txt_nota.append(f'{codigo_produto:0<6} {"":-<4} {produto: <10} {"":-<4} {quantidade:0>3} {"-" * 4} {preco: >5}')
 
     def adicionar_nota():
         #
@@ -67,19 +114,81 @@ def main(ui):
         #         adicionar_linha_nota(codigo,produto,quantidade,preco)
         global codigo, produto, preco, lista_nota
 
-
         quantidade = str(ui.txt_quantidade.text()).strip()
-        try:
-            adicionar_linha_nota(codigo,produto,quantidade,preco)
-            definir_preco_total(preco,quantidade)
-            if ui.rd_dinheiro.isChecked():
-                print('a')
-        except:
-            print('erro')
+        if len(ui.txt_codigo_produto.text()) != 0:
+            try:
+                adicionar_linha_nota(codigo,produto,quantidade,preco)
+                definir_preco_total(preco,quantidade)
+            except ValueError as erro:
+                mostrar_msg('Caixa EMPRESA XYZ',f'Erro nome: {erro}')
+        else:
+            mostrar_msg('Caixa EMPRESA XYZ','Digite um produto antes de inserir')
+
+    def limpar_campos():
+        ui.txt_preco.clear()
+        ui.txt_codigo_produto.clear()
+        ui.txt_produto.clear()
+        ui.txt_preco_unitario.clear()
+        ui.txt_quantidade.clear()
+        ui.txt_codigo.clear()
+        ui.txt_remover_produto.clear()
+        ui.txt_dinheiro_recebido.clear()
+        ui.txt_nota.clear()
+
+    def enviar_dados():
+            definir_troco()
+            valor_total = float(ui.txt_preco.text())
+            dinheiro_recebido = float(ui.txt_dinheiro_recebido.text())
+            troco = float(ui.txt_troco.text())
+            nota = ui.txt_nota.toPlainText()
+            print('CODIGO ---- NOME PRODU ---- QTD ----  PRECO')
+            print(nota)
+            print('')
+            print('')
+            print('/'*54)
+            print('')
+            print(f'Total: {valor_total:<5.2f} --- Dinheiro: {dinheiro_recebido:<5.2f} ---- Troco: {troco:<5.2f}')
+            print('')
+            print('/'*54)
 
 
+
+    def verificar_campos():
+        botao_cartao = ui.rd_cartao.isChecked()
+        botao_dinheiro = ui.rd_dinheiro.isChecked()
+        valor_retorno = 1
+        if len(ui.txt_nota.toPlainText()) == 0:
+            mostrar_msg('CAIXA EMPRESA XYZ','Entre ao Menos Um Item')
+            valor_retorno = 0
+        if botao_cartao == False and botao_dinheiro == False:
+            mostrar_msg('CAIXA EMPRESA XYZ','Selecione o Tipo de Pagamento')
+            valor_retorno = 0
+        elif ui.rd_dinheiro.isChecked() == True and len(ui.txt_dinheiro_recebido.text()) == 0:
+            mostrar_msg('CAIXA EMPRESA XYZ','Digite o Valor Recebido')
+            valor_retorno = 0
+        if len(ui.txt_dinheiro_recebido.text()) != 0 and len(ui.txt_preco.text()) != 0:
+            recebido = float(ui.txt_dinheiro_recebido.text())
+            total = float(ui.txt_preco.text())
+            troco = recebido - total
+            if troco < 0:
+                mostrar_msg('CAIXA EMPRESA XYZ', 'O valor Recebido Não pode ser menor que o Valor total')
+                valor_retorno = 0
+        return valor_retorno
+
+
+
+    def finalizar_compra():
+        verificar = verificar_campos()
+        if verificar == 1:
+            # print('a')
+            enviar_dados()
+            limpar_campos()
+
+    ui.lb_caixa.setText('01')
     ui.btn_adicionar_produto.clicked.connect(adicionar_nota)
     ui.btn_procurar.clicked.connect(procurar)
+    ui.btn_finalizar_compra.clicked.connect(finalizar_compra)
+    ui.rd_cartao.clicked.connect(lambda: ui.txt_dinheiro_recebido.setText(''))
 
 #     Mascaras de campos de texto
 
--- SQLite/Projetos/Caixa_Supermercado/teste.py
+++ SQLite/Projetos/Caixa_Supermercado/teste.py
@@ -1,13 +1,13 @@
-produtos = ['000001','Pipoca','01','2,59','000002','Manteiga','01','4,20']
-produtos = list(produtos)
-pesquisar = '1'
-if len(pesquisar) < 6:
-    pesquisar = ('0' * (6 - len(pesquisar))) + pesquisar
-codigo_indice = produtos.index(f'{pesquisar}')
-codigo = produtos[codigo_indice]
-produto = produtos[codigo_indice+1]
-quantidade = produtos[codigo_indice+2]
-preco = produtos[codigo_indice+3]
+# produtos = ['000001','Pipoca','01','2,59','000002','Manteiga','01','4,20']
+# produtos = list(produtos)
+# pesquisar = '1'
+# if len(pesquisar) < 6:
+#     pesquisar = ('0' * (6 - len(pesquisar))) + pesquisar
+# codigo_indice = produtos.index(f'{pesquisar}')
+# codigo = produtos[codigo_indice]
+# produto = produtos[codigo_indice+1]
+# quantidade = produtos[codigo_indice+2]
+# preco = produtos[codigo_indice+3]
 # print(f'Codigo {codigo}')
 # print(f'Produto {produto}')
 # print(f'Quantidade {quantidade}')
@@ -17,38 +17,44 @@ preco = produtos[codigo_indice+3]
 # print(f'{codigo:0<6} {"-"*7} {produto:^13} {"-"*4} {quantidade:^3} {"-"*3} {preco:^7}')
 # 6 7 13 4 3 7
 # XXXXXX ------- XXXXXXXXXXXXX ---- XXX ---- XXXXXXX
-
-
-print(produtos)
-
+#
+#
+# print(produtos)
+#
 # deletar
-
+#
 # del produtos[codigo_indice+3]
 # del produtos[codigo_indice+2]
 # del produtos[codigo_indice+1]
 # del produtos[codigo_indice]
 # print(produtos)
-
+#
 # criar nota
-
-for x in range(1,(len(produtos)//4)+1):
-    if len(str(x)) < 6:
-        pesquisar = ('0' * (6 - len(str(x)))) + str(x)
-    codigo_indice = produtos.index(f'{pesquisar}')
-    codigo = produtos[codigo_indice]
-    produto = produtos[codigo_indice + 1]
-    quantidade = produtos[codigo_indice + 2]
-    preco = produtos[codigo_indice + 3]
-    # print(f'Codigo {codigo}')
-    # print(f'Produto {produto}')
-    # print(f'Quantidade {quantidade}')
-    # print(f'Preco {preco}')
-    #
-    # print('CODIGO ------- NOME PRODUTO  ---- QTD --- PREÇO')
-    print(f'{codigo:0<6} {"-"*7} {produto:^13} {"-"*4} {quantidade:^3} {"-"*3} {preco:^7}')
-    # 6 7 13 4 3 7
-    # XXXXXX ------- XXXXXXXXXXXXX ---- XXX ---- XXXXXXX
-
-
-
-
+#
+# for x in range(1,(len(produtos)//4)+1):
+#     if len(str(x)) < 6:
+#         pesquisar = ('0' * (6 - len(str(x)))) + str(x)
+#     codigo_indice = produtos.index(f'{pesquisar}')
+#     codigo = produtos[codigo_indice]
+#     produto = produtos[codigo_indice + 1]
+#     quantidade = produtos[codigo_indice + 2]
+#     preco = produtos[codigo_indice + 3]
+#     # print(f'Codigo {codigo}')
+#     # print(f'Produto {produto}')
+#     # print(f'Quantidade {quantidade}')
+#     # print(f'Preco {preco}')
+#     #
+#     # print('CODIGO ------- NOME PRODUTO  ---- QTD --- PREÇO')
+#     print(f'{codigo:0<6} {"-"*7} {produto:^13} {"-"*4} {quantidade:^3} {"-"*3} {preco:^7}')
+#     # 6 7 13 4 3 7
+#     # XXXXXX ------- XXXXXXXXXXXXX ---- XXX ---- XXXXXXX
+# from builtins import range
+# from datetime import datetime
+#
+# def data_agora():
+#     data = str(datetime.today())
+#     data = data[:-7]
+#     return data
+#
+# print(data_agora())
+# datetime.date()
