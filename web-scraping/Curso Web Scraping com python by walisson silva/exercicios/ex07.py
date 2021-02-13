e8d4b5e41ce0ef54fbda5162f4ba0ded1b52be11
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2021-02-13 00:13:35 -0300

    Update ex07.py

--- web-scraping/Curso Web Scraping com python by walisson silva/exercicios/ex07.py
+++ web-scraping/Curso Web Scraping com python by walisson silva/exercicios/ex07.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import sqlite3
 from datetime import datetime
 
+#  BUSCANDO COTAÇÕES MOEDAS DO INFOMONEY
 
 data = datetime.now().date()
 resposta = requests.get('https://www.infomoney.com.br/ferramentas/cambio/')
@@ -23,6 +24,25 @@ moeda = bloco.find_all('tr')
 cotacoes = []
 dic = {}
 
+
+
+try:
+    banco = sqlite3.connect('banco_dados_cotacoes.db')
+    cursor = banco.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS cotacoes( 
+    id integer PRIMARY KEY AUTOINCREMENT,
+    moeda text,
+    compra double,
+    venda double,
+    variacao double,
+    data date   
+    )
+    ''')
+    banco.commit()
+except:
+    pass
+
+
 for x in moeda[1:]:
 
     # print(moeda[2].prettify())
@@ -38,10 +58,24 @@ for x in moeda[1:]:
     cotacoes.append(dic.copy())
     dic.clear()
 
+
+
+
 for x in cotacoes:
-    print(f'Moeda  : {x["nome_moeda"]}')
-    print(f'Compra : {x["compra_moeda"]}')
-    print(f'Venda  : {x["venda_moeda"]}')
-    print(f'Var    : {x["var_moeda"]}')
-    print()
+    try:
+        banco = sqlite3.connect('banco_dados_cotacoes.db')
+        cursor = banco.cursor()
+        cursor.execute(f'''INSERT INTO cotacoes VALUES (NULL,'{x['nome_moeda']}','{x['compra_moeda']}','{x['venda_moeda']}','{x['var_moeda']}','{data}')''')
+        banco.commit()
+        banco.close()
+
+    except sqlite3.Error as erro:
+        print(f'Erro de nome: {erro}')
+    #
+    # print(f'Moeda  : {x["nome_moeda"]}')
+    # print(f'Compra : {x["compra_moeda"]}')
+    # print(f'Venda  : {x["venda_moeda"]}')
+    # print(f'Var    : {x["var_moeda"]}')
+    # print()
+
 
