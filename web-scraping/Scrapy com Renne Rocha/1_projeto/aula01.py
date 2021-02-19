d3102d1a58234e2f8d45441261d3169424407a60
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2021-02-19 13:47:22 -0300

    Aulas Sobre scrapy

--- web-scraping/Scrapy com Renne Rocha/1_projeto/aula01.py
+++ web-scraping/Scrapy com Renne Rocha/1_projeto/aula01.py
@@ -1,1 +1,22 @@
 import scrapy
+
+# Primeiro Web Scraping com Scrapy
+# Site:
+# Objetivo: Entrar no site aosfatos e verificar todas as noticias buscando, o titulo, data, citação, verdadeira ou falsa
+
+class AosFatosSpider(scrapy.Spider):
+    name = 'aos_fatos'
+
+    start_urls = ['https://aosfatos.org/']
+
+    def parse(self, response):
+        links = response.xpath('//nav//ul//li/a[re:test(@href, "checamos")]/@href').getall()
+        for link in links:
+            yield scrapy.Request(
+                response.urljoin(link),
+                callback=self.parse_category
+            )
+
+    def parse_category(self, response):
+        import ipdb; ipdb.set_trace()
+        pass
