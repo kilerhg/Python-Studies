4caf63f398fdc31ead1c374834b6c4f364b7dc7f
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2021-02-27 02:07:01 -0300

    Aulas curso flask

---
+++ Flask/Curso de Flask by Julia Rizza/aula03/app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+
+from app.controllers import default
---
+++ Flask/Curso de Flask by Julia Rizza/aula03/app/models/tables.py
@@ -0,0 +1,46 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    passowrd = db.Column(db.String)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    user_id = db.Column(dm.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', foreign_keys=user_id)
+
+    def __init__(self, content, user_id):
+        self.content = content
+        self.user_id = user_id
+
+    def __repr(self):
+        return f"<Post {self.id}>"
+
+class Follow(db.Model):
+    __tablename__ = "follow"
+
+    id = db.column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', foreign_keys=user_id)
+    follower = db.relationship('User', foreign_keys=follower_id)
