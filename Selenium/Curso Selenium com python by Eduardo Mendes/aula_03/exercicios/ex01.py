b4bbf3b35341605f257c1c9c4baa4821deecea9e
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2021-04-15 16:33:21 -0300

    Resolução exercicios Selenium

--- Selenium/Curso Selenium com python by Eduardo Mendes/aula_03/exercicios/ex01.py
+++ Selenium/Curso Selenium com python by Eduardo Mendes/aula_03/exercicios/ex01.py
@@ -1,5 +1,40 @@
 '''
-Crie um programa em selenium que: Gere um dicionario onde a chave e a tag h1
+Crie um programa em selenium que: Gere um dicionario onde a chave é a tag h1
 O valor deve ser um novo dicionario, cada chave do valor devera ser o valor de atributo, cada valor deve ser o texto contido no elemento
 url : https://curso-python-selenium.netlify.app/exercicio_01.html
+
+Estrutura = {'H1':{'texto1':'Essa página é top de mais','texto2':'texto','texto3':'texto'}}
+
+
 '''
+
+# p.get_attribute('atributo')
+
+from selenium.webdriver import Firefox
+from time import sleep
+
+navegador = Firefox()
+
+url = 'https://curso-python-selenium.netlify.app/exercicio_01.html'
+
+navegador.get(url)
+
+sleep(3)
+
+dicionario = {}
+dicionario['H1'] = {}
+
+elementos = navegador.find_elements_by_tag_name('p')
+for elemento in elementos:
+    chave = elemento.get_attribute('atributo')
+    valor = elemento.text
+    dicionario['H1'][chave] = valor
+
+print(dicionario)
+
+
+navegador.quit()
+
+
+
+
--- Selenium/Curso Selenium com python by Eduardo Mendes/aula_03/exercicios/ex02.py
+++ Selenium/Curso Selenium com python by Eduardo Mendes/aula_03/exercicios/ex02.py
@@ -1,2 +1,38 @@
+'''
 Crie um programa em selenium que: jogue o jogo, quando voce ganha o script deve parar de ser executado
 url : https://curso-python-selenium.netlify.app/exercicio_02.html
+'''
+
+
+
+from selenium.webdriver import Firefox
+from time import sleep
+
+navegador = Firefox()
+
+url = 'https://curso-python-selenium.netlify.app/exercicio_02.html'
+
+navegador.get(url)
+
+sleep(3)
+
+geral = navegador.find_elements_by_tag_name('p')[1:]
+botao = navegador.find_element_by_tag_name('a')
+num_esperado = eval(geral[0].text[-1:])
+
+a = 'asdasd'
+
+cont = 0
+while True:
+    cont += 1
+    botao.click()
+    num_atual = navegador.find_elements_by_tag_name('p')[-1].text
+    print(f'Numero Atual: {num_atual}, Numero Esperado: {num_esperado}')
+    #if len(num_atual) > 2: Forma N1
+    if eval(num_atual[-1]) == num_esperado:
+        break
+
+print(f'Você Ganhou !! : Esperado {num_esperado}, você {num_atual[-1]}')
+
+
+
