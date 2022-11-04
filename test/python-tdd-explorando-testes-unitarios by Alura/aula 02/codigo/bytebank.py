0bd1208eef92c74991120ba4fbd7aedff32b6f0f
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2022-11-04 00:08:56 -0300

    feat: PyTest Studies

--- test/python-tdd-explorando-testes-unitarios by Alura/aula 02/codigo/bytebank.py
+++ test/python-tdd-explorando-testes-unitarios by Alura/aula 02/codigo/bytebank.py
@@ -15,8 +15,8 @@ class Funcionario:
         return self._salario
 
     def idade(self):
-        data_nasciemnto_quebrada = self._data_nascimento.split('/')
-        ano_nascimento = data_nasciemnto_quebrada[-1]
+        data_nascimento_quebrada = self._data_nascimento.split('/')
+        ano_nascimento = data_nascimento_quebrada[-1]
         ano_atual = date.today().year
         return ano_atual - int(ano_nascimento)
     
