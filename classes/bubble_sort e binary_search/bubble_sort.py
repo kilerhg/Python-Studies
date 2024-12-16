01aa31fecac982705fde17d919d9c28be1a8407f
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2024-12-15 22:05:08 -0300

    feat(classes): binary search and bubble_sort example

--- classes/bubble_sort e binary_search/bubble_sort.py
+++ classes/bubble_sort e binary_search/bubble_sort.py
@@ -8,15 +8,15 @@ def bubble_sort(array):
         print()
         all_valid = []
         for pos in range(number_items):
-            if pos+1 < number_items:
+            if pos+1 < number_items: # fazendo validação para não buscar um elemento que não existe (Index error)
                 print(f"comparando: {sorting_array[pos]} > {sorting_array[pos+1]} - {sorting_array[pos] > sorting_array[pos+1]}")
                 if sorting_array[pos] > sorting_array[pos+1]:
                     print(sorting_array)
-                    sorting_array[pos], sorting_array[pos+1] = sorting_array[pos+1], sorting_array[pos]
+                    sorting_array[pos], sorting_array[pos+1] = sorting_array[pos+1], sorting_array[pos] # Invertendo o numero caso seja maior
                     all_valid.append(False)
                 else:
                     all_valid.append(True)
-        all_sorted = all(all_valid)
+        all_sorted = all(all_valid) # Verificando se nessa iteração nenhuma alteração foi feita
     return sorting_array
 
 array_unsorted = [1, 7, 3, 2, 4, 6, 5, 8, 9, 10]
